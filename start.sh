2bdd942d0c27996664d2c9cd7dfe814049e783a0
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-01-16 14:47:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/soulpc/AUTO-FILTER-V2.git /AUTO-FILTER-V2 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /AUTI-FILTER-V2 
 fi
 cd /VJ-FILTER-BOT 
 pip3 install -U -r requirements.txt
