c30b736df35d49d7916116c9a052ac9c772b1d66
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-01-16 15:32:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AUTO-FILTER-V2
 fi
-cd /Elsa
+cd /AUTO-FILTER-V2
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
