b54b704f2b76e73a6d79bd9177b84716c64a4db4
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-01-16 16:39:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/soulpc/AUTO-FILTER-V2.git /AUTO-FILTER-V2
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AUTO-FILTER-V2
-fi
-cd /AUTO-FILTER-V2
-pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
-python3 bot.py
+FROM python:3.10.8-slim-buster
+
+RUN apt update && apt upgrade -y
+RUN apt install git -y
+COPY requirements.txt /requirements.txt
+
+RUN cd /
+RUN pip3 install -U pip && pip3 install -U -r requirements.txt
+WORKDIR /AUTO-FILTER-V2
+
+COPY . .
+
+CMD ["python3", "bot.py"]
